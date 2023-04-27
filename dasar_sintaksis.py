eb62c7ac426ef5b7a422125d871ff415140b53d7
Hasan Gani Rachmatullah <ganirh0612@gmail.com>
2023-04-27 14:36:07 +0700

    dasar sintaksis

---
+++ dasar_sintaksis.py
@@ -0,0 +1,14 @@
+milk_bottle_stock = 13
+egg_stock = 12
+
+print(f"Jumlah Botol Susu {milk_bottle_stock} botol")
+
+#branches
+if milk_bottle_stock >= 1 :
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak membeli apa - apa")
+
+print("Budi pulang kerumah")
+print("Menyampaikan hasil belanja kepada ibu")
+
